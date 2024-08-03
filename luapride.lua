a4c3eeb88cf727da958320815830b96b099fdc99
Benjamin Voisin <benjamin.voisin@ens-rennes.fr>
2024-08-03 14:24:19 +0100

    ✨ now can display various flags.
    "Inclusive" by default, else the flag told by the first argument

--- luapride.lua
+++ luapride.lua
@@ -2,23 +2,40 @@
 
 local rl = require("ffi/raylib")
 
-
 local windowWidth = 800
 local windowHeight = 450
 
 rl.InitWindow(windowWidth, windowHeight, "LuaPride")
 rl.SetTargetFPS(60)
 
-local flag_image = rl.LoadImage("./flags/inclusive.png")
+local flags = {
+	ace = {
+		path = "./flags/ace.png",
+	},
+	aro = {
+		path = "./flags/aro.png"
+	},
+	inclusive = {
+		path = "./flags/inclusive.png"
+	},
+	pan = {
+		path = "./flags/pansexual.png"
+	}
+}
 
-rl.ImageResize(flag_image, windowWidth, windowHeight)
+local flag = arg[1] or "inclusive"
 
-local flag = rl.LoadTextureFromImage(flag_image)
+-- Load images
+for key, value in pairs(flags) do
+	flags[key].image = rl.LoadImage(value.path)
+	rl.ImageResize(flags[key].image, windowWidth, windowHeight)
+	flags[key].texture = rl.LoadTextureFromImage(flags[key].image)
+end
 
 while not rl.WindowShouldClose() do
 	rl.BeginDrawing()
 	rl.ClearBackground(rl.RAYWHITE)
-	rl.DrawTexture(flag, 0, 0, rl.WHITE)
+	rl.DrawTexture(flags[flag].texture, 0, 0, rl.WHITE)
 	rl.EndDrawing()
 end
 
