b0c520cd20d3bafff9ed5a3b1d59781fa01bbbb4
Benjamin Voisin <benjamin.voisin@ens-rennes.fr>
2024-08-04 01:05:40 +0100

    ⚡️ make easy flags into raylib shapes and no png

--- luapride/ffi/colors.lua
+++ luapride/ffi/colors.lua
@@ -2,7 +2,21 @@ local ffi = require("ffi")
 
 return {
 	-- For the Bi flag
-	bi_magenta = ffi.new("Color",  214, 2, 112, 255),   -- White
-	bi_purple = ffi.new("Color",  155, 79, 150, 255),   -- White
-	bi_blue = ffi.new("Color",  0, 56, 168, 255),   -- White
+	bi_magenta = ffi.new("Color",  214, 2, 112, 255),
+	bi_purple = ffi.new("Color",  155, 79, 150, 255),
+	bi_blue = ffi.new("Color",  0, 56, 168, 255),
+
+	-- For the Aro flag
+	aro_green = ffi.new("Color",  61, 165, 66, 255),
+	aro_pale_green = ffi.new("Color",  167, 211, 121, 255),
+	aro_gray = ffi.new("Color",  169, 169, 169, 255),
+
+	-- For the Ace flag
+	ace_gray = ffi.new("Color",  163, 163, 163, 255),
+	ace_purple = ffi.new("Color",  128, 0, 128, 255),
+
+	-- For the Pan flag
+	pan_pink = ffi.new("Color",  255, 33, 140, 255),
+	pan_yellow = ffi.new("Color",  255, 216, 0, 255),
+	pan_cyan = ffi.new("Color",  22, 177, 255, 255),
 }
--- luapride/flags.lua
+++ luapride/flags.lua
@@ -3,20 +3,35 @@ local colors = require("luapride/ffi/colors")
 
 return {
 	ace = {
-		type = "png",
-		path = "./flags/ace.png",
+		type = "raylib",
+		draw = function (width, height)
+			rl.DrawRectangle(0,0, width, height / 4 + 1, rl.BLACK)
+			rl.DrawRectangle(0,(height * 1) / 4, width, height / 4 + 1, colors.ace_gray)
+			rl.DrawRectangle(0,(height * 2) / 4, width, height / 4 + 1, rl.WHITE)
+			rl.DrawRectangle(0,(height * 3) / 4, width, height / 4 + 1, colors.ace_purple)
+		end
 	},
 	aro = {
-		type = "png",
-		path = "./flags/aro.png"
+		type = "raylib",
+		draw = function (width, height)
+			rl.DrawRectangle(0,0, width, height / 5 + 1, colors.aro_green)
+			rl.DrawRectangle(0,height / 5, width, height / 5 + 1, colors.aro_pale_green)
+			rl.DrawRectangle(0,(height * 2) / 5, width, height / 5 + 1, rl.WHITE)
+			rl.DrawRectangle(0,(height * 3) / 5, width, height / 5 + 1, colors.aro_gray)
+			rl.DrawRectangle(0,(height * 4) / 5, width, height / 5 + 1, rl.BLACK)
+		end
 	},
 	inclusive = {
 		type = "png",
 		path = "./flags/inclusive.png"
 	},
 	pan = {
-		type = "png",
-		path = "./flags/pansexual.png"
+		type = "raylib",
+		draw = function (width, height)
+			rl.DrawRectangle(0,0, width, height / 3 + 1, colors.pan_pink)
+			rl.DrawRectangle(0,(height * 1) / 3, width, height / 3 + 1, colors.pan_yellow)
+			rl.DrawRectangle(0,(height * 2) / 3, width, height / 3 + 1, colors.pan_cyan)
+		end
 	},
 	bi = {
 		type = "raylib",
